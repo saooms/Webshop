c5df3352e81886767ad874f7817cffdae439b4b9
saooms <saooms@hotmail.com>
2019-03-20 11:56:03 +0100

    laravel setup, authorisation setup, strugling with migrations and foreinkey's

---
+++ Webshop/database/migrations/2019_03_20_094817_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('price');
+            $table->integer('category_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('articles', function (Blueprint $table){
+            $table->foreign('category_id')->references('id')->on('Categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
---
+++ Webshop/database/migrations/2019_03_20_094958_create_order_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('orders_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('order_details', function (Blueprint $table){
+            $table->foreign('orders_id')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
